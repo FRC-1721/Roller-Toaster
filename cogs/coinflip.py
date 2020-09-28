38c18f94cf29611944ac74a31e8ba9ccada43cbf
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-09-28 19:23:41 -0400

    Add comments to coin flip

--- cogs/coinflip.py
+++ cogs/coinflip.py
@@ -5,14 +5,14 @@ from discord.ext import commands
 
 class CoinFlip(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot = bot # Pass in the bot object locally
 
-    @commands.command(name='flip')
-    async def cool_bot(self, chan):
-        if (random.randint(0, 1) == 0):
-            await chan.send('Heads')
+    @commands.command(name='flip') # This is a command, its name is flip
+    async def coinflip(self, ctx): # Pass in self and ctx (data about what triggered this cog)
+        if (random.randint(0, 1) == 0): # Randomize
+            await ctx.send('Heads')
         else:
-            await chan.send("Tails")
+            await ctx.send("Tails")
 
-def setup(bot):
-    bot.add_cog(CoinFlip(bot))
+def setup(bot): # Gets passed when created
+    bot.add_cog(CoinFlip(bot)) # Add this cog
